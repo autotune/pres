606efc248451585d02e1b70911af3c5901c64acb
Brian Adams <b@contrasting.org>
2016-03-23 22:12:59 -0700

    Moved terraform to own folder

--- terraform/ami-asps2.pilot.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ami_from_instance" "apses2" {
-    provider = "aws.ap-southeast-2"
-    name = "apses2-ami"
-    source_instance_id = "${aws_instance.prod_pub_apses2.id}"
-    depends_on = ["aws_instance.prod_pub_apses2"]
-}
